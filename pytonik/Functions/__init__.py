5e901f4279967114817b491f5ef9d1e4f3aa34eb
Emmanuel Martins <emmamartinscm@gmail.com>
2020-03-14 16:12:52 -0500

    Add files via upload

--- pytonik/Functions/__init__.py
+++ pytonik/Functions/__init__.py
@@ -1,10 +1,16 @@
-# Author : BetaCodings
+###
+# Author : Betacodings
 # Author : info@betacodings.com
 # Maintainer By: Emmanuel Martins
 # Maintainer Email: emmamartinscm@gmail.com
-# Created by BetaCodings on 13/01/2020.
+# Created by Betacodings on 2019.
+###
+
+
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from pytonik.cmd.start import main
-from pytonik.cmd.server import main
-from pytonik.cmd.doc import main
+
+from pytonik.cmd import start
+from pytonik.cmd import server
+from pytonik.cmd import install
+from pytonik.cmd import doc
