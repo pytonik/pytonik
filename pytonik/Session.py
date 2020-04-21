1aa2bb9da690b182acb2d63c5215cddae107d5eb
Emmanuel Essien <emmamartinscm@gmail.com>
2020-04-21 02:22:17 +0100

    pytonik

--- pytonik/Session.py
+++ pytonik/Session.py
@@ -20,10 +20,7 @@ except Exception as err:
     import Cookie as cook
 
 
-
-
 class Session(Variable):
-
     def __getattr__(self, item):
         return item
 
@@ -34,62 +31,65 @@ class Session(Variable):
         self.result = ""
         self.s_string = "HTTP_COOKIE"
         self.session_list = []
+        self._commit = []
+        self.url_v = self.out("HTTP_HOST") + str(":") + str(self.out("SERVER_PORT", '')) if self.out(
+            "HTTP_HOST") == "localhost" or self.out("HTTP_HOST") == "127.0.0.1" else self.out("HTTP_HOST")
         return None
 
     def has(self, key=""):
-
+        bool_v = False
         if self.out("SERVER_SOFTWARE") == Version.AUTHOR:
             session_dict = self.x_get()
             if len(session_dict) > 0:
                 if session_dict.get(key, "") != "":
-                    return True
+                    bool_v = True
                 elif session_dict.get(' {key}'.format(key=key), "") != "":
-                    return True
+                    bool_v = True
                 else:
-                    return False
+                    bool_v = False
             else:
-                return False
+                bool_v = False
         else:
 
             if self.get(key) != "" and self.get(key) != None:
 
-                return True
+                bool_v = True
             else:
-                return False
+                bool_v = False
 
+        return bool_v
 
     def set(self, key="", value="", duration=3600, url="", path="/"):
-        url_v = self.out("HTTP_HOST")+str(":")+str(self.out("SERVER_PORT", '')) if self.out("HTTP_HOST") == "localhost" or self.out("HTTP_HOST") == "127.0.0.1" else self.out("HTTP_HOST")
-        url = url if url  != "" else url_v
-        expires = datetime.datetime.utcnow() + datetime.timedelta(minutes=duration)  # minutes in 30 days
 
         if self.out("SERVER_SOFTWARE") == Version.AUTHOR:
-            session_string = str(key)+"="+str(value)
+            session_string = str(key) + "=" + str(value)
             self.set_x(session_string)
         else:
+            self.set_k(key, value, duration, url, path)
 
-            cooKeys = cook.SimpleCookie(self.out(self.s_string))
+    def set_k(self, key="", value="", duration=3600, url="", path="/"):
+        url = url if url != "" else self.url_v
+        expires = datetime.datetime.utcnow() + datetime.timedelta(minutes=duration)  # minutes in 30 days
+        cooKeys = cook.SimpleCookie(self.out(self.s_string))
+        cooKeys[str(key)] = value
+        cooKeys[str(key)]['domain'] = url
+        cooKeys[str(key)]['path'] = '/'
+        cooKeys[str(key)]['expires'] = expires.strftime('%a, %d %b %Y %H:%M:%S')
 
-            cooKeys[str(key)] = value
-            cooKeys[str(key)]['domain'] = url
-            cooKeys[str(key)]['path'] = '/'
-            cooKeys[str(key)]['expires'] = expires.strftime('%a, %d %b %Y %H:%M:%S')
-
-            print(cooKeys)
-
-
-
+        print(cooKeys[str(key)])
 
     def get(self, key=""):
         if self.out("SERVER_SOFTWARE") == Version.AUTHOR:
             session_dict = self.x_get()
+
             if len(session_dict) > 0:
                 if session_dict.get(key, "") != None or session_dict.get(key, "") != "":
                     try:
                         return ast.literal_eval(session_dict.get(key, ""))
                     except Exception as err:
                         return session_dict.get(key, "")
-                elif session_dict.get(' {key}'.format(key=key), "") != None or session_dict.get(' {key}'.format(key=key), "") != "":
+                elif session_dict.get(' {key}'.format(key=key), "") != None or session_dict.get(
+                        ' {key}'.format(key=key), "") != "":
                     try:
                         return ast.literal_eval(session_dict.get(' {key}'.format(key=key), ""))
                     except Exception as err:
@@ -121,15 +121,15 @@ class Session(Variable):
                 return ""
 
     def destroy(self, *args):
-
-
+        bool_v = False
         if self.out("SERVER_SOFTWARE") == Version.AUTHOR:
 
             session_result = self._delete(args)
             if session_result[0] == True:
-                return self._reset(session_result[1])
+                self._reset(session_result[1])
+                bool_v = True
             else:
-                return False
+                bool_v = False
 
         else:
             cooKeys = cook.SimpleCookie(self.out(self.s_string))
@@ -141,24 +141,37 @@ class Session(Variable):
 
             OsEnviron = self.out(self.s_string)
 
+            bool_vx = False
             if self.s_string in os.environ:
                 if args:
 
                     for key in args:
 
                         if self.get(key) != "":
-                            return self.set(key, "", 60)
+                            duration = 3600 * 30 * 1000
+                            self.set_k(key=key, duration=-duration)
+                            bool_vx = True
+                    bool_v = bool_vx
+
                 else:
+
                     for key, v in cookv:
 
                         if self.get(key) != "":
-                            return self.set(key, "", 60)
+                            duration = 3600 * 33 * 1000
+
+                            self.set_k(key=key, duration=-duration)
+                            bool_vx = True
+
+                    bool_v = bool_vx
+
 
             else:
-                return False
+                bool_v = False
+        return bool_v
 
     def set_x(self, session_string=""):
-        if session_string  != "":
+        if session_string != "":
             if self.s_string not in self.see():
                 self.default(self.s_string, session_string)
 
@@ -168,23 +181,23 @@ class Session(Variable):
         else:
             return False
 
-
     def _update(self, session_string):
-        dict_session = dict({"HTTP_COOKIE": session_string})
+        dict_session = dict({self.s_string: session_string})
         try:
             self.update(dict_session)
             return True
         except Exception as e:
             return False
 
-
     def unqiue(self, session_list=[]):
 
         session_relist = []
         for l_session in session_list:
-            session_string = str(l_session)+";"+str(self.out("HTTP_COOKIE", ""))
+            session_string = str(l_session) + ";" + str(self.out(self.s_string, ""))
             session_relist.append(session_string)
-        initial_session = str(session_relist).replace("[' ", "").replace("]", "").replace(',', ";").replace("'", "").replace("[", "")
+        initial_session = str(session_relist).replace("[' ", "").replace("]", "").replace(',', ";").replace("'",
+                                                                                                            "").replace(
+            "[", "")
         return self._unquie(initial_session)
 
     def _unquie(self, initial_session=""):
@@ -197,7 +210,6 @@ class Session(Variable):
             return initial_session
 
     def _delete(self, session_key=tuple()):
-
 
         get_session = self.x_get()
         response = None
@@ -223,16 +235,16 @@ class Session(Variable):
             response = False
         return response, get_session
 
-    def _reset(self, session_dict = dict()):
+    def _reset(self, session_dict=dict()):
 
-        session_list =  []
+        session_list = []
         if Version.PYVERSION_MA >= 3:
             session_dict_l = session_dict.items()
         else:
             session_dict_l = session_dict.iteritems()
 
         for k, v in session_dict_l:
-            session_list.append("{k}={v}".format(k=k,v=v))
+            session_list.append("{k}={v}".format(k=k, v=v))
 
         return self._update(";".join(session_list))
 
@@ -248,11 +260,11 @@ class Session(Variable):
         try:
             for c_g in session_string.split(";"):
                 k, v = c_g.split("=")
-                session_dict.update({str(k):str(v)})
+                session_dict.update({str(k): str(v)})
         except Exception as err:
             try:
                 k, v = session_string.split("=")
-                session_dict.update({str(k):str(v)})
+                session_dict.update({str(k): str(v)})
             except Exception as err:
-                 session_dict = {}
+                session_dict = {}
         return session_dict
