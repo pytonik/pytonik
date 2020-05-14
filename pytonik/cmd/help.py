12d2ba943acb60cb30d6640be5d86e0d2e4c627a
Raphael Essien <emmaessiensp@gmail.com>
2020-05-14 11:22:13 -0500

    Add files via upload

--- pytonik/cmd/help.py
+++ pytonik/cmd/help.py
@@ -5,6 +5,11 @@
 # Created by BetaCodings on 1/19/20.
 
 
+
+import cgitb
+import base64
+import traceback
+import socket
 import argparse
 import locale
 import os
@@ -12,17 +17,24 @@ import re
 import sys
 import time
 import warnings
-import traceback
-from pytonik import Version
 from pytonik.cmd import lang
-from pytonik.Core.requirement import requirement
-from typing import Any, Callable, Dict, List, Pattern, Union
-from random import randint
+from pytonik.cmd import start
+from pytonik.cmd import server
+from pytonik.cmd import install
+from pytonik.cmd import doc
+from pytonik import Version
+from pytonik import serv 
+from pytonik.Functions.url import url
 from typing import Any, Callable, Dict, List, Pattern, Union
 from pytonik.cmd.console import (  # type: ignore
     colorize, bold, red, green, turquoise, nocolor, color_terminal
 )
-from random import randint
+
+
+import webbrowser
+
+cgitb.enable()
+
 
 try:
     import readline
@@ -35,7 +47,6 @@ try:
         USE_LIBEDIT = False
 except ImportError:
     USE_LIBEDIT = False
-
 
 
 class ValidationError(Exception):
@@ -66,7 +77,6 @@ def nonempty(x: str) -> str:
 
 
 def __(mes_id):
-
     try:
         userlang = locale.getlocale()[0]
 
@@ -76,7 +86,7 @@ def __(mes_id):
                 getla = lang.lang.get(l, '')
 
                 if getla != "":
-                    l_ =  getla.get(mes_id, '')
+                    l_ = getla.get(mes_id, '')
                     if l_ != "":
                         l_ = getla.get(mes_id, '')
                     else:
@@ -87,6 +97,7 @@ def __(mes_id):
         l_ = mes_id
     return l_
 
+
 def doTraceBack():
     exc_type, exc_value, exc_traceback = sys.exc_info()
     traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
@@ -95,9 +106,9 @@ def doTraceBack():
     traceback.print_exc()
 
 
-
 if sys.platform == 'win32':
-    # On Windows, show questions as bold because of color scheme of PowerShell (refs: #5294).
+    # On Windows, show questions as bold because of color scheme of PowerShell
+    # (refs: #5294).
     COLOR_QUESTION = 'bold'
 else:
     COLOR_QUESTION = 'purple'
@@ -108,6 +119,7 @@ PROMPT_PREFIX = '> '
 def do_prompt(text: str, default: str = None, validator: Callable[[str], Any] = nonempty) -> Union[str, bool]:  # NOQA
 
     while True:
+
         if default is not None:
             prompt = PROMPT_PREFIX + '%s [%s]: ' % (text, default)
         else:
@@ -131,25 +143,6 @@ def do_prompt(text: str, default: str = None, validator: Callable[[str], Any] = 
     return x
 
 
-def ask(d: Dict) -> None:
-
-    print(bold(__('Welcome to the Pytonik MVC Framework %s.' % Version.VERSION_TEXT)))
-
-    print(__(bold(red('PLEASE NOTE :'))+' This feature is use for installation of project requirments.'))
-
-    d['step'] = do_prompt(__('Do you want install requirements  (y/n)'),'n', boolean)
-    if d.get('step', '') == True:
-        try:
-            requirement().rf()
-        except Exception as err:
-            print(err)
-
-    if d.get('step', '') is False:
-        sys.exit(-1)
-
-
-
-
 def term_input(prompt: str) -> str:
     if sys.platform == 'win32':
 
@@ -162,24 +155,88 @@ def term_input(prompt: str) -> str:
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         usage='%(prog)s [OPTIONS] <PROJECT_DIR>',
-        epilog=__("For more information, visit < https://pytonik.readthedocs.io >."),
+        epilog=__(
+            "For more information, visit < https://pytonik.readthedocs.io >."),
         description=__("""Pytonik is a python framework built to enhance web development
         fast and easy, also help web developers to build more apps with less codes.
         it uses expressive architectural pattern, structured on model view controller MVC
-        and bundles of component to reuse while deploying the framework."""))
+        and bundles of component to reuse while deploying the framework.
+        
+        
+        """))
 
     parser.add_argument('-q', '--quit', action='store_true', dest='quit',
                         default=None,
                         help=__('quit mode'))
-    parser.add_argument('--version', action='version', dest='show_version',
-                        version='%%(prog)s %s' % Version.VERSION_TEXT)
 
-    parser.add_argument('path', metavar='PROJECT_DIR', default='.', nargs='?',
-                        help=__('project root'))
+    parser.add_argument('--version', '-v', action='version', dest='show_version',
+                        version='Pytonik %s' % Version.VERSION_TEXT)
 
+    parser.add_argument('start', metavar='PROJECT_DIR', default='all', nargs='?',
+                        help=__('Terminal Command'))
+    
     return parser
 
 
+def ask(d: Dict) -> None:
+    
+    if len(d) > 0:
+        if d.get('start', '') == 'start':
+            return start.main(sys.argv[1:])
+
+        if d.get('start', '') == 'server':
+            return server.main(sys.argv[1:])
+
+        if d.get('start', '') == 'install' :
+            return install.main(sys.argv[1:])
+        
+        if d.get('start', '') == 'version' :
+            print('Pytonik %s' % Version.VERSION_TEXT)
+        
+        if d.get('start', '') == 'doc' or d.get('start', '') =="docs":
+            return doc.main(sys.argv[1:])
+        
+        if d.get('start', '') == 'quit':
+            return sys.exit(-1)
+        
+
+        if d.get('start', '') == 'all':
+            return help_notes()
+
+        if d.get('quit', '') is True:
+            return sys.exit(-1)
+            
+    else:
+        return help_notes()
+    
+
+def help_notes():
+    cnt_text = """
+    Pytonik is a python framework built to enhance web development
+    fast and easy, also help web developers to build more apps with less codes.
+    it uses expressive architectural pattern, structured on model view controller MVC
+    and bundles of component to reuse while deploying the framework.
+
+    usage: 
+    Options and arguments (and corresponding environment variables):
+
+    pytonik [option]
+    ================
+    start     			: Create new pytonik project to an assign directory
+    server     			: Run pytonik Server from an assign project directory
+    install 			: Install pytonik requirement from an assign directory
+    doc     			: Read Pytonik Documentation (doc) or (docs)
+    version     		: Pytonik Version (-v) or (--version)
+
+    direct command [pytonik-option]
+    ===============================
+    pytonik-start     	: Create a new pytonik project to an assign directory
+    pytonik-server     	: Run pytonik Server from an assign project directory
+    pytonik-install     : Install pytonik requirement from an assign directory
+    pytonik-docs     	: Read Pytonik Documentation
+    """
+
+    print(cnt_text)
 
 def main(argv: List[str] = sys.argv[1:]) -> int:
     # parse options
@@ -191,9 +248,6 @@ def main(argv: List[str] = sys.argv[1:]) -> int:
         return err.code
     d = vars(args)
     ask(d)
-    #path = str(os.getcwd())
-
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
