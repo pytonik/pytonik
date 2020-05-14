8441359001c3ffa433f2b7e9b824e44d5bd2d659
Raphael Essien <emmaessiensp@gmail.com>
2020-05-14 11:16:48 -0500

    Add files via upload

--- pytonik/__init__.py
+++ pytonik/__init__.py
@@ -9,3 +9,4 @@ from pytonik.cmd import start
 from pytonik.cmd import server
 from pytonik.cmd import doc
 from pytonik.cmd import install 
+from pytonik.cmd import help
--- pytonik/__main__.py
+++ pytonik/__main__.py
@@ -10,6 +10,8 @@ import sys
 from pytonik.cmd import start
 from pytonik.cmd import server
 from pytonik.cmd import doc
+from pytonik.cmd import install
+from pytonik.cmd import help
 
 if __name__ == '__main__':
     sys.exit(start.main(sys.argv[1:]))
@@ -19,3 +21,10 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     sys.exit(doc.main(sys.argv[1:]))
+
+if __name__ == '__main__':
+    sys.exit(install.main(sys.argv[1:]))
+
+if __name__ == '__main__':
+    sys.exit(help.main(sys.argv[1:]))
+
