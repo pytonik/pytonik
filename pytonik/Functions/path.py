1c4a058aa9ca2dd8c1914754479bedd4a25e838c
Raphael Essien <emmaessiensp@gmail.com>
2020-08-03 13:47:39 +0100

    Update path.py

--- pytonik/Functions/path.py
+++ pytonik/Functions/path.py
@@ -10,13 +10,11 @@ from pytonik.util.Variable import Variable
 from pytonik import Version
 
 if os.path.isdir(os.getcwd() + '/public'):
-    host_public = str(os.getcwd()).replace('\\', '/') + '/public' # os.path.dirname(os.getcwd())
+    host_public = str(os.getcwd()).replace('\\', '/') + '/public'  # os.path.dirname(os.getcwd())
     host_app = str(os.path.dirname(os.getcwd())).replace("\\", "/") + '/app'
 else:
     host_public = str(os.getcwd()).replace("\\", "/")
     host_app = str(os.path.dirname(os.getcwd())).replace("\\", "/")
-
-
 
 
 class path(url, Variable):
@@ -46,60 +44,67 @@ class path(url, Variable):
         u = ""
         DS = "/"
         dev_path = ""
+    
+        if bool(link) == True:
+            u = self.url()
+
         if path[:1] == DS or path[0] == DS:
             DS = ""
         else:
             DS = "/"
-
-        if bool(link) == True:
-            u = self.url()
-
+    
         return str(u)+str(DS)+str(path)
 
     def exist(self, newpath, defaultpath="", link=False):
+
         newpath = "" if newpath == None or newpath == "" else newpath
         path_res = ""
-        if os.path.isfile(newpath) == True:
 
-            path_res = self.path(path=newpath, link=link)
+        try:
+            if os.path.isfile(newpath) == True:
 
-        elif os.path.isfile(host_app + "/" + str(newpath)) == True:
+                path_res = self.path(path=newpath, link=link)
 
-            path_res = self.path(path=newpath, link=link)
+            elif os.path.isfile(str(host_app)+"/"+str(newpath)) == True:
 
-        elif os.path.isfile(host_public + "/" + str(newpath)) == True:
+                path_res = self.path(path=newpath, link=link)
 
-            path_res = self.path(path="/public/"+str(+newpath), link=link)
+            elif os.path.isfile(str(host_public)+"/" + str(newpath)) == True:
 
-        elif os.path.isdir(newpath) == True:
-            path_res =  newpath
+                path_res = self.path(path=str("/public/")+str(+newpath), link=link)
+                
+            elif os.path.isdir(newpath) == True:
+                path_res = newpath
 
-        elif os.path.isdir(self.public(path=newpath)) == True:
-            path_res =  self.public(path=newpath)
+            elif os.path.isdir(self.public(path=newpath)) == True:
+                path_res = self.public(path=newpath)
 
-        else:
+            else:
 
-            if defaultpath != "":
+                if defaultpath != "":
 
-                if os.path.isfile(defaultpath) == True:
-                    path_res = self.path(path=defaultpath, link=link)
+                    if os.path.isfile(defaultpath) == True:
+                        path_res = self.path(path=defaultpath, link=link)
 
-                elif os.path.isfile(host_app + "/" + str(defaultpath)) == True:
+                    elif os.path.isfile(str(host_app) + "/" + str(defaultpath)) == True:
 
-                    path_res = self.path(path=defaultpath, link=link)
+                        path_res = self.path(path=defaultpath, link=link)
 
-                elif os.path.isfile(host_public + "/" + str(defaultpath)) == True:
+                    elif os.path.isfile(str(host_public)+"/"+str(defaultpath)) == True:
 
-                    path_res = self.path(path="/public/"+str(+defaultpath), link=link)
+                        path_res = self.path(path=str("/public/")+str(defaultpath), link=link)
 
-                elif os.path.isdir(defaultpath) == True:
-                    path_res =  defaultpath
+                    elif os.path.isdir(defaultpath) == True:
+                        path_res = defaultpath
 
-                elif os.path.isdir(self.public(defaultpath)) == True:
+                    elif os.path.isdir(self.public(defaultpath)) == True:
 
-                    path_res =  self.public(path=defaultpath)
+                        path_res = self.public(path=defaultpath)
+            return path_res
 
-        return path_res
+        except Exception as err:
+
+            return err
 
     def public(self, path):
         DS = "/"
